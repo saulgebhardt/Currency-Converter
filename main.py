271dcb9d9cd5da057080b6f8fc64aa4b1d6cd81d
Saul Gebhardt <saul.gebhardt@tiscali.nl>
2024-05-08 13:44:59 +0200

    Convert from Euros to Baht

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 ### This program is a CLI program that allows you to convert an amount of dollars to euros.
 import requests
-from datetime import date
+from datetime import datetime
 import sys
 
 url = "https://api.exchangerate-api.com/v4/latest/EUR"
@@ -8,15 +8,15 @@ codes = "https://v6.exchangerate-api.com/v6/codes"
 
 def main(args):
     response = requests.get(url)
-    response_codes = requests.get(codes)
     ### Load in data from url
     data = response.json()
-    currency_codes = response_codes.json()
-    print(currency_codes)
     original_value = int(args[1])
+    print(data['time_last_updated'])
+    human_readable_date = datetime.fromtimestamp((data['time_last_updated'])).strftime('%d-%m-%Y')
+
     conversion_rate = data['rates']['THB']
     new_value = original_value * conversion_rate
-    print(original_value , " Euros is worth " , new_value ,  " Thai Baht on " , data['date'])
+    print(original_value , "Euros is worth" , new_value ,  "Thai Baht on" , human_readable_date)
 
 if __name__ == '__main__':
     main(sys.argv)
